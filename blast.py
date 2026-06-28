519bd39af1e3b2fa7473664170ffe3fbe69d976f
Jayde Howell <jayde.m.howell@gmail.com>
2026-06-27 18:06:26 -0700

    make explosions more lumpy

--- blast.py
+++ blast.py
@@ -1,17 +1,44 @@
 import pygame
+import math
+import random
 from circleshape import CircleShape
+from constants import BLAST_VERTICES
 
 class Blast(CircleShape):
     def __init__(self, x: float, y: float, max_radius: float):
-        super().__init__(x, y, 0)
+        super().__init__(x, y, radius=20)
         self.max_radius = max_radius
+        self.time_integral = 0.0
+        self.phase_velocity = random.uniform(2.0, 8.0)
+        self.spatial_frequency = random.randint(7, 14)
+        self.amplitude = random.randint(10, 20)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "red", self.position, self.radius, width=0)
+        points = self.get_polar()
+        pygame.draw.polygon(screen, "red", points, width=0)
 
     
     def update(self, dt):
+        self.time_integral += dt
         self.radius += (self.max_radius * dt)
         if self.radius >= self.max_radius:
             self.kill()
 
+
+    def get_polar(self) -> list:
+        coordinates = []
+
+        step_size = 2 * math.pi / BLAST_VERTICES
+
+        for i in range(BLAST_VERTICES):
+
+            theta = i * step_size
+            blast_radius = self.radius + self.amplitude * math.sin(self.spatial_frequency * theta + self.phase_velocity * self.time_integral)
+
+            x = self.position.x + blast_radius * math.cos(theta)
+            y = self.position.y + blast_radius * math.sin(theta)
+
+            coordinates.append((x, y))
+
+
+        return coordinates
